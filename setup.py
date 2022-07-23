02c99f71c6a64d24bbb49b1692321e62a6352451
Bernhard <b_walter@arcor.de>
2022-07-23 17:04:39 +0200

    Latest release: 1.0.0rc1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup_args = {
     "name": "cadquery_massembly",
-    "version": "1.0.0rc0",
+    "version": "1.0.0rc1",
     "description": "A manual assembly system for cadquery based on mates",
     "include_package_data": True,
     "install_requires": [],
