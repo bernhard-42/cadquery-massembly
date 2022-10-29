326ce111bb78547004f0b7d70daf9facea2b8281
Bernhard <b_walter@arcor.de>
2022-10-29 17:54:58 +0200

    ported basic assembly functions

--- examples/build123d/hexapod.py
+++ examples/build123d/hexapod.py
@@ -1,4 +1,11 @@
-from cadquery_massembly.build123d import BuildAssembly, Mates, Part, Mate, Assemble
+from cadquery_massembly.build123d import (
+    BuildAssembly,
+    Mates,
+    Part,
+    Mate,
+    Assemble,
+    Color,
+)
 from build123d import *
 from cq_vscode import show, show_object
 
@@ -14,12 +21,12 @@ class Base:
     def __init__(self):
         x1, x2 = 0.63, 0.87
         self.base_hinges = {
-            "right_back": {"pos": (-x1 * length, -x1 * width), "angle": -105},
-            "right_middle": {"pos": (0, -x2 * width), "angle": -90},
-            "right_front": {"pos": (x1 * length, -x1 * width), "angle": -75},
-            "left_back": {"pos": (-x1 * length, x1 * width), "angle": 105},
-            "left_middle": {"pos": (0, x2 * width), "angle": 90},
-            "left_front": {"pos": (x1 * length, x1 * width), "angle": 75},
+            "right_back": (-x1 * length, -x1 * width),
+            "right_middle": (0, -x2 * width),
+            "right_front": (x1 * length, -x1 * width),
+            "left_back": (-x1 * length, x1 * width),
+            "left_middle": (0, x2 * width),
+            "left_front": (x1 * length, x1 * width),
         }
         self.base_edges = {}
 
@@ -27,20 +34,21 @@ class Base:
             "front_stand": (0.75 * length, 0),
             "back_stand": (-0.8 * length, 0),
         }
-        self.stand_edges = {}
+        self.stand_wires = {}
+
         self.obj = None
 
     def create(self):
-        with BuildPart() as base:
+        with BuildPart(Plane.YX) as base:
             with BuildSketch():
                 Ellipse(length, width)
             Extrude(amount=thickness)
 
-            with Locations((length - 5, 0, 0)):
+            with Locations((-length + 5, 0, 0)):
                 Box(20, 2 * width, 3 * thickness, mode=Mode.SUBTRACT)
 
             for name, pos in self.base_hinges.items():
-                with Locations(pos["pos"]):
+                with Locations(pos):
                     Cylinder(
                         diam / 2 + tol,
                         thickness,
@@ -57,26 +65,21 @@ class Base:
                         5 * thickness,
                         mode=Mode.SUBTRACT,
                     )
-                self.stand_edges[name] = base.edges(Select.LAST).sort_by()[0]
+                self.stand_wires[name] = Wire.make_wire(
+                    base.edges(Select.LAST).group_by()[0], sequenced=True
+                )
 
         self.obj = base
         return base
 
-    def mates(self, names):
-        m = []
-        if "holes" in names:
-            m += [
-                Mate(edge, name=name).rotated((0, 0, self.base_hinges[name]["angle"]))
-                for name, edge in self.base_edges.items()
-            ]
-        if "base" in names:
-            m += [
-                Mate(self.obj.faces().sort_by()[-1], name="base").translated(
-                    (0, 0, height + 2 * tol)
-                )
-            ]
-        if "top" in names:
-            m += [Mate(self.obj.faces().sort_by()[-1], name="top")]
+    def mates(self):
+        m = {name: Mate(edge, name=name) for name, edge in self.base_edges.items()}
+        m2 = {name: Mate(edge, name=name) for name, edge in self.stand_wires.items()}
+        m.update(m2)
+        m["base"] = Mate(self.obj.faces().sort_by()[-1], name="base").translated(
+            (0, 0, height + 2 * tol)
+        )
+        m["top"] = Mate(self.obj.faces().sort_by()[0], name="top")
         return m
 
 
@@ -85,29 +88,30 @@ class Stand:
         self.h = 5
 
     def create(self):
-        with BuildPart() as stand:
-            b = Box(height, width / 2 + 10, thickness)
-            faces = b.faces().sort_by(Axis.Y)
+        with BuildPart(Plane.YX) as stand:
+            b = Box(width / 2 + 10, height + 2 * tol, thickness)
+            faces = b.faces().sort_by(Axis.X)
 
             t2 = thickness / 2
-            w = height / 2 - self.h / 4
+            w = height / 2 + tol - self.h / 2
             with Locations((0, -w, t2), (0, w, t2)):
                 with BuildSketch():
                     Rectangle(thickness, self.h)
                 Extrude(amount=self.h)
 
             Chamfer(
-                *stand.edges().group_by()[-1].group_by(Axis.Y)[-3], length=self.h - tol
-            )
-            Chamfer(
-                *stand.edges().group_by()[-1].group_by(Axis.Y)[-4],
+                *stand.edges().group_by()[0].sort_by(Axis.X)[3:5],
                 length=self.h - 2 * tol,
             )
 
             with Workplanes(faces[0], faces[-1]):
                 Box(thickness, width / 2, thickness, centered=(True, True, False))
 
+        self.obj = stand
         return stand
+
+    def mates(self):
+        return {"bottom": Mate(self.obj.faces().sort_by(Axis.X)[0], name="bottom")}
 
 
 class UpperLeg:
@@ -120,19 +124,19 @@ class UpperLeg:
         points = [(0, 0), (0, height / 2), (self.l1, height / 2 - 5), (self.l2, 0)]
         upper_leg_hole = (self.l2 - 10, 0)
 
-        with BuildPart() as upper_leg:
+        with BuildPart(Plane.YX) as upper_leg:
             with BuildSketch():
                 with BuildLine():
                     Polyline(*points)
                     Mirror(about=Plane.XZ)
                 BuildFace()
             Extrude(amount=thickness)
-            Fillet(upper_leg.edges().sort_by(Axis.X)[-1], radius=4)
+            Fillet(upper_leg.edges().sort_by(Axis.Y)[-1], radius=4)
 
             with Locations(upper_leg_hole):
                 Hole(diam / 2 + tol)
 
-            with Workplanes(Plane((0, 0, thickness / 2), (1, 0, 0), ((0, 1, 0)))):
+            with Workplanes(Plane((0, 0, -thickness / 2), (0, 1, 0), ((1, 0, 0)))):
                 Cylinder(
                     diam / 2,
                     2 * (height / 2 + thickness + tol),
@@ -141,15 +145,18 @@ class UpperLeg:
         self.obj = upper_leg
         return upper_leg
 
-    def mates(self, prefix):
-        o = -1 if "left" in prefix else 0
-        return [
-            Mate(upper_leg.faces().sort_by(Axis.Y)[0], name=f"{prefix}_hinge"),
-            Mate(
-                upper_leg.faces().sort_by()[o].edges().sort_by(Axis.X)[-2],
-                name=f"{prefix}_knee",
-            ),
-        ]
+    def mates(self):
+        suffix = ["top", "bottom"]
+        return {
+            "hinge": Mate(self.obj.faces().sort_by(Axis.X)[0], name=f"hinge"),
+            "knee": [
+                Mate(
+                    self.obj.edges().group_by(Axis.Y)[-3].sort_by()[ind],
+                    name=f"knee_{suffix[ind]}",
+                )
+                for ind in [0, 1]
+            ],
+        }
 
 
 class LowerLeg:
@@ -163,7 +170,7 @@ class LowerLeg:
         points = [(0, 0), (self.l1, self.w), (self.l2, 0)]
         upper_leg_hole = (self.l1 - 10, 0)
 
-        with BuildPart() as lower_leg:
+        with BuildPart(Plane.YX) as lower_leg:
             with BuildSketch():
                 with BuildLine():
                     Polyline(*points)
@@ -178,14 +185,16 @@ class LowerLeg:
         self.obj = lower_leg
         return lower_leg
 
-    def mates(self, prefix):
-        o = 0 if "left" in prefix else -1
-        return [
-            Mate(
-                self.obj.faces().sort_by()[o].edges().sort_by(Axis.X)[3],
-                name=f"{prefix}_lower_knee",
-            ).rotated((0, 0, -75))
-        ]
+    def mates(self):
+        return {
+            "knee": [
+                Mate(
+                    self.obj.edges().group_by(Axis.Y)[4].sort_by()[ind],
+                    name=f"knee",
+                )
+                for ind in [0, 1]
+            ]
+        }
 
 
 _base = Base()
@@ -194,11 +203,11 @@ show_object(base, name="base", clear=True)
 
 _stand = Stand()
 stand = _stand.create()
-show_object(stand.part.translate((0, 100, 0)), name="stand")
+show_object(stand.part.translate((100, 0, 0)), name="stand")
 
 _upper_leg = UpperLeg()
 upper_leg = _upper_leg.create()
-show_object(upper_leg.part.translate((20, -100, 0)), name="upper_leg")
+show_object(upper_leg.part.translate((-100, 20, 0)), name="upper_leg")
 
 _lower_leg = LowerLeg()
 lower_leg = _lower_leg.create()
@@ -206,25 +215,64 @@ show_object(lower_leg.part.translate((-100, -100, 0)), name="lower_leg")
 
 
 # %%
+
+
+def base_rot(mate):
+    angles = {
+        "right_back": -75,
+        "right_middle": -90,
+        "right_front": -105,
+        "left_back": 75,
+        "left_middle": 90,
+        "left_front": 105,
+    }
+    angle = angles.get(mate.name, 0)
+    if angle == 0:
+        return mate
+    else:
+        return mate.rotated((0, 180, angle))
+
+
 with BuildAssembly(name="hexapod") as a:
 
-    with Mates(*_base.mates(["holes", "base"])):
-        Part(base, name="base")
+    with Mates(*[base_rot(v) for k, v in _base.mates().items() if k != "top"]):
+        Part(base, name="base", color=Color("gray"))
 
-    with Mates(*_base.mates(["top"])):
-        Part(base, name="top")
+    with Mates(_base.mates()["top"]):
+        Part(base, name="top", color=Color(204,204,204))
+
+    for name in _base.stand_holes.keys():
+        with Mates(_stand.mates()["bottom"].rename(f"{name}_bottom")):
+            Part(stand, name=name, color=Color(128, 204, 230))
 
     for name in _base.base_hinges.keys():
         with BuildAssembly(name=f"{name}_leg"):
-            with Mates(*_upper_leg.mates(name)):
+
+            m = _upper_leg.mates()
+            ind = 1 if "left" in name else 0
+            with Mates(
+                m["knee"][ind].rename(f"{name}_knee"),
+                m["hinge"].rename(f"{name}_hinge"),
+            ):
                 Part(upper_leg, name=f"{name}_upper")
 
-            with Mates(*_lower_leg.mates(name)):
+            m = _lower_leg.mates()
+            ind = 0 if "left" in name else 1
+            with Mates(
+                m["knee"][ind].rotated((0, 0, -75)).rename(f"{name}_lower_knee")
+            ):
                 Part(lower_leg, name=f"{name}_lower")
 
         Assemble(f"{name}_hinge", name)
         Assemble(f"{name}_lower_knee", f"{name}_knee")
+
+    for pos in ["front", "back"]:
+        Assemble(f"{pos}_stand_bottom", f"{pos}_stand")
+
     Assemble("top", "base")
 
 show(a, render_mates=True, mate_scale=5, reset_camera=True)
+# %%
+
+
 # %%
